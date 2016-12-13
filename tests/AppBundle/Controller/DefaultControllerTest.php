dc29ba2e57390b320391c5ad937a0fd3b2e4e2c6
Nicolas Lœuillet <nicolas.loeuillet@inopale.fr>
2016-12-13 10:46:50 +0100

    Added test for like/unlike

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -8,7 +8,7 @@ class DefaultControllerTest extends AppTestCase
 {
     public function testIndexLogout()
     {
-    	$client = $this->getClient();
+        $client = $this->getClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
@@ -17,12 +17,42 @@ class DefaultControllerTest extends AppTestCase
 
     public function testIndexLogged()
     {
-    	$this->logInAs('nicolas');
+        $this->logInAs('nicolas');
 
-		$client = $this->getClient();
+        $client = $this->getClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Logout', $client->getResponse()->getContent());
+    }
+
+    public function testJaimeJaimeplus()
+    {
+        $this->logInAs('nicolas');
+
+        $client = $this->getClient();
+        $crawler = $client->request('GET', '/');
+
+        // Chercher dans la page un message dont le texte est "j'aime"
+        $link = $crawler
+            ->filter('a:contains("J\'aime")')
+            ->eq(0)
+            ->link()
+        ;
+        $crawler = $client->click($link);
+        $crawler = $client->followRedirect();
+
+        $this->assertNotContains("J'aime", $client->getResponse()->getContent());
+
+        // Je remets les données comme elles l'étaient au début du test
+        $link = $crawler
+            ->filter('a:contains("aime plus")')
+            ->eq(0)
+            ->link()
+        ;
+        $client->click($link);
+        $client->followRedirect();
+
+        $this->assertNotContains('aime plus', $client->getResponse()->getContent());
     }
 }
